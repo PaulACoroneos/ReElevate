0cb36883d003c68e056daadda853a5a96f8b7996
thomas <thomasdang93@gmail.com>
2018-10-07 00:50:24 -0500

    updated sql script and added schema.png

--- database/disastermanagement.sql
+++ database/disastermanagement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 07, 2018 at 01:46 AM
+-- Generation Time: Oct 07, 2018 at 05:44 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -21,6 +21,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `disastermanagement`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE IF NOT EXISTS `address` (
+  `address_id` int(8) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,26 +64,8 @@ DROP TABLE IF EXISTS `contact_info`;
 CREATE TABLE IF NOT EXISTS `contact_info` (
   `email` varchar(255) NOT NULL,
   `phone` int(11) NOT NULL,
-  `social_media_fk` int(8) NOT NULL,
-  `contact_id` int(11) NOT NULL,
-  PRIMARY KEY (`contact_id`),
-  KEY `social_media_fk` (`social_media_fk`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `date`
---
-
-DROP TABLE IF EXISTS `date`;
-CREATE TABLE IF NOT EXISTS `date` (
-  `date_id` int(8) NOT NULL,
-  `day_fk` int(8) NOT NULL,
-  `hour_fk` int(8) NOT NULL,
-  PRIMARY KEY (`date_id`),
-  KEY `day_fk` (`day_fk`),
-  KEY `hour_fk` (`hour_fk`)
+  `contact_id` int(8) NOT NULL,
+  PRIMARY KEY (`contact_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -105,9 +102,10 @@ CREATE TABLE IF NOT EXISTS `disaster_type` (
 
 DROP TABLE IF EXISTS `donation_form`;
 CREATE TABLE IF NOT EXISTS `donation_form` (
-  `donor_id` int(11) NOT NULL,
+  `donor_id` int(8) NOT NULL,
   `amount` float NOT NULL,
   `donor_type_fk` int(8) NOT NULL,
+  `donor_name` varchar(255) NOT NULL,
   PRIMARY KEY (`donor_id`),
   KEY `donor_type_fk` (`donor_type_fk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -129,28 +127,15 @@ CREATE TABLE IF NOT EXISTS `donor_type` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hour`
---
-
-DROP TABLE IF EXISTS `hour`;
-CREATE TABLE IF NOT EXISTS `hour` (
-  `hour_id` int(8) NOT NULL,
-  `hour_type` varchar(20) NOT NULL,
-  PRIMARY KEY (`hour_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `local_resource_form`
 --
 
 DROP TABLE IF EXISTS `local_resource_form`;
 CREATE TABLE IF NOT EXISTS `local_resource_form` (
-  `local_resource_form_id` int(11) NOT NULL,
+  `local_resource_form_id` int(8) NOT NULL,
   `location_scope` text NOT NULL,
-  `resource_provider_fk` int(11) NOT NULL,
-  `contact_info_fk` int(11) NOT NULL,
+  `resource_provider_fk` int(8) NOT NULL,
+  `contact_info_fk` int(8) NOT NULL,
   `disaster_fk` int(8) NOT NULL,
   PRIMARY KEY (`local_resource_form_id`),
   KEY `resource_provider_fk` (`resource_provider_fk`),
@@ -167,16 +152,31 @@ CREATE TABLE IF NOT EXISTS `local_resource_form` (
 DROP TABLE IF EXISTS `organization_form`;
 CREATE TABLE IF NOT EXISTS `organization_form` (
   `organization_id` int(8) NOT NULL,
-  `organization_name` varchar(255) NOT NULL,
-  `headquarters` varchar(255) NOT NULL,
+  `organization_name_fk` int(8) NOT NULL,
   `location_scope` text NOT NULL,
   `disaster_fk` int(8) NOT NULL,
   `resource_fk` int(8) NOT NULL,
   `contact_fk` int(8) NOT NULL,
+  `address_fk` int(8) NOT NULL,
   PRIMARY KEY (`organization_id`),
   KEY `disaster_fk` (`disaster_fk`),
   KEY `resource_fk` (`resource_fk`),
-  KEY `contact_fk` (`contact_fk`)
+  KEY `contact_fk` (`contact_fk`),
+  KEY `address_fk` (`address_fk`),
+  KEY `organization_name_fk` (`organization_name_fk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organization_name`
+--
+
+DROP TABLE IF EXISTS `organization_name`;
+CREATE TABLE IF NOT EXISTS `organization_name` (
+  `org_id` int(8) NOT NULL,
+  `org_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`org_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -195,6 +195,19 @@ CREATE TABLE IF NOT EXISTS `resource_provider` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `resource_type`
+--
+
+DROP TABLE IF EXISTS `resource_type`;
+CREATE TABLE IF NOT EXISTS `resource_type` (
+  `resource_id` int(8) NOT NULL,
+  `resource_type` varchar(255) NOT NULL,
+  PRIMARY KEY (`resource_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `skill`
 --
 
@@ -208,66 +221,33 @@ CREATE TABLE IF NOT EXISTS `skill` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `social_media`
---
-
-DROP TABLE IF EXISTS `social_media`;
-CREATE TABLE IF NOT EXISTS `social_media` (
-  `social_id` int(8) NOT NULL,
-  `social_platform` varchar(255) NOT NULL,
-  `social_account` varchar(255) NOT NULL,
-  PRIMARY KEY (`social_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `volunteer_form`
 --
 
 DROP TABLE IF EXISTS `volunteer_form`;
 CREATE TABLE IF NOT EXISTS `volunteer_form` (
   `volunteer_id` int(8) NOT NULL,
-  `contact_fk` int(11) NOT NULL,
+  `contact_fk` int(8) NOT NULL,
   `location_scope` text NOT NULL,
-  `organization_fk` int(8) NOT NULL,
-  `date_fk` int(8) NOT NULL,
-  `skill_fk` int(11) NOT NULL,
+  `skill_fk` int(8) NOT NULL,
+  `password` int(8) NOT NULL,
+  `street_address` varchar(255) NOT NULL,
+  `day_fk` int(8) NOT NULL,
+  `organization_name_fk` int(8) NOT NULL,
+  `resource_fk` int(8) NOT NULL,
+  `recruited` char(1) NOT NULL DEFAULT 'N',
+  `volunteer_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`volunteer_id`),
   KEY `contact_fk` (`contact_fk`),
-  KEY `organization_fk` (`organization_fk`),
-  KEY `date_fk` (`date_fk`),
-  KEY `skill_fk` (`skill_fk`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `volunteer_resource`
---
-
-DROP TABLE IF EXISTS `volunteer_resource`;
-CREATE TABLE IF NOT EXISTS `volunteer_resource` (
-  `resource_id` int(8) NOT NULL,
-  `resource_type` varchar(255) NOT NULL,
-  PRIMARY KEY (`resource_id`)
+  KEY `skill_fk` (`skill_fk`),
+  KEY `day` (`day_fk`),
+  KEY `organization_name_fk` (`organization_name_fk`),
+  KEY `resource_fk` (`resource_fk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `contact_info`
---
-ALTER TABLE `contact_info`
-  ADD CONSTRAINT `contact_info_ibfk_1` FOREIGN KEY (`social_media_fk`) REFERENCES `social_media` (`social_id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `date`
---
-ALTER TABLE `date`
-  ADD CONSTRAINT `date_ibfk_1` FOREIGN KEY (`day_fk`) REFERENCES `day` (`day_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `date_ibfk_2` FOREIGN KEY (`hour_fk`) REFERENCES `hour` (`hour_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `donation_form`
@@ -288,17 +268,20 @@ ALTER TABLE `local_resource_form`
 --
 ALTER TABLE `organization_form`
   ADD CONSTRAINT `organization_form_ibfk_1` FOREIGN KEY (`disaster_fk`) REFERENCES `disaster_type` (`disaster_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `organization_form_ibfk_2` FOREIGN KEY (`resource_fk`) REFERENCES `volunteer_resource` (`resource_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `organization_form_ibfk_3` FOREIGN KEY (`contact_fk`) REFERENCES `contact_info` (`contact_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `organization_form_ibfk_2` FOREIGN KEY (`resource_fk`) REFERENCES `resource_type` (`resource_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `organization_form_ibfk_3` FOREIGN KEY (`contact_fk`) REFERENCES `contact_info` (`contact_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `organization_form_ibfk_4` FOREIGN KEY (`address_fk`) REFERENCES `address` (`address_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `organization_form_ibfk_5` FOREIGN KEY (`organization_name_fk`) REFERENCES `organization_name` (`org_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `volunteer_form`
 --
 ALTER TABLE `volunteer_form`
   ADD CONSTRAINT `volunteer_form_ibfk_1` FOREIGN KEY (`contact_fk`) REFERENCES `contact_info` (`contact_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `volunteer_form_ibfk_2` FOREIGN KEY (`organization_fk`) REFERENCES `organization_form` (`organization_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `volunteer_form_ibfk_3` FOREIGN KEY (`date_fk`) REFERENCES `date` (`date_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `volunteer_form_ibfk_4` FOREIGN KEY (`skill_fk`) REFERENCES `skill` (`skill_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `volunteer_form_ibfk_4` FOREIGN KEY (`skill_fk`) REFERENCES `skill` (`skill_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `volunteer_form_ibfk_5` FOREIGN KEY (`day_fk`) REFERENCES `day` (`day_id`),
+  ADD CONSTRAINT `volunteer_form_ibfk_6` FOREIGN KEY (`organization_name_fk`) REFERENCES `organization_name` (`org_id`),
+  ADD CONSTRAINT `volunteer_form_ibfk_7` FOREIGN KEY (`resource_fk`) REFERENCES `resource_type` (`resource_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
