1b937aa725a811476eeb8474790beed15d47dfd5
thomas <thomasdang93@gmail.com>
2018-10-07 13:37:18 -0500

    added database schema.png and sql file

--- database/disastermanagement.sql
+++ database/disastermanagement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 07, 2018 at 05:44 AM
+-- Generation Time: Oct 07, 2018 at 06:02 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -35,6 +35,29 @@ CREATE TABLE IF NOT EXISTS `address` (
   `state` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_form`
+--
+
+DROP TABLE IF EXISTS `business_form`;
+CREATE TABLE IF NOT EXISTS `business_form` (
+  `local_resource_form_id` int(8) NOT NULL,
+  `location_scope` text NOT NULL,
+  `contact_info_fk` int(8) NOT NULL,
+  `disaster_fk` int(8) NOT NULL,
+  `business_name` varchar(255) NOT NULL,
+  `business_type` varchar(255) NOT NULL,
+  `resource_fk` int(8) NOT NULL,
+  `business_fax` int(11) NOT NULL,
+  PRIMARY KEY (`local_resource_form_id`),
+  KEY `contact_info_fk` (`contact_info_fk`),
+  KEY `disaster_fk` (`disaster_fk`),
+  KEY `resource_fk` (`resource_fk`),
+  KEY `business_fax` (`business_fax`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -118,29 +141,10 @@ CREATE TABLE IF NOT EXISTS `donation_form` (
 
 DROP TABLE IF EXISTS `donor_type`;
 CREATE TABLE IF NOT EXISTS `donor_type` (
-  `donor_id` int(11) NOT NULL,
-  `volunteer_fk` int(8) NOT NULL,
-  `organization_fk` int(8) NOT NULL,
+  `donor_id` int(8) NOT NULL,
+  `volunteer_option` int(8) NOT NULL,
+  `organization_option` int(8) NOT NULL,
   PRIMARY KEY (`donor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `local_resource_form`
---
-
-DROP TABLE IF EXISTS `local_resource_form`;
-CREATE TABLE IF NOT EXISTS `local_resource_form` (
-  `local_resource_form_id` int(8) NOT NULL,
-  `location_scope` text NOT NULL,
-  `resource_provider_fk` int(8) NOT NULL,
-  `contact_info_fk` int(8) NOT NULL,
-  `disaster_fk` int(8) NOT NULL,
-  PRIMARY KEY (`local_resource_form_id`),
-  KEY `resource_provider_fk` (`resource_provider_fk`),
-  KEY `contact_info_fk` (`contact_info_fk`),
-  KEY `disaster_fk` (`disaster_fk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -177,19 +181,6 @@ CREATE TABLE IF NOT EXISTS `organization_name` (
   `org_id` int(8) NOT NULL,
   `org_name` varchar(255) NOT NULL,
   PRIMARY KEY (`org_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `resource_provider`
---
-
-DROP TABLE IF EXISTS `resource_provider`;
-CREATE TABLE IF NOT EXISTS `resource_provider` (
-  `resource_provider_id` int(8) NOT NULL,
-  `resource_provider_type` varchar(255) NOT NULL,
-  PRIMARY KEY (`resource_provider_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -237,12 +228,14 @@ CREATE TABLE IF NOT EXISTS `volunteer_form` (
   `resource_fk` int(8) NOT NULL,
   `recruited` char(1) NOT NULL DEFAULT 'N',
   `volunteer_name` varchar(255) NOT NULL,
+  `address_fk` int(8) NOT NULL,
   PRIMARY KEY (`volunteer_id`),
   KEY `contact_fk` (`contact_fk`),
   KEY `skill_fk` (`skill_fk`),
   KEY `day` (`day_fk`),
   KEY `organization_name_fk` (`organization_name_fk`),
-  KEY `resource_fk` (`resource_fk`)
+  KEY `resource_fk` (`resource_fk`),
+  KEY `address_fk` (`address_fk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -250,18 +243,18 @@ CREATE TABLE IF NOT EXISTS `volunteer_form` (
 --
 
 --
+-- Constraints for table `business_form`
+--
+ALTER TABLE `business_form`
+  ADD CONSTRAINT `business_form_ibfk_1` FOREIGN KEY (`contact_info_fk`) REFERENCES `contact_info` (`contact_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `business_form_ibfk_3` FOREIGN KEY (`disaster_fk`) REFERENCES `disaster_type` (`disaster_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `business_form_ibfk_4` FOREIGN KEY (`resource_fk`) REFERENCES `resource_type` (`resource_id`);
+
+--
 -- Constraints for table `donation_form`
 --
 ALTER TABLE `donation_form`
   ADD CONSTRAINT `donation_form_ibfk_1` FOREIGN KEY (`donor_type_fk`) REFERENCES `donor_type` (`donor_id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `local_resource_form`
---
-ALTER TABLE `local_resource_form`
-  ADD CONSTRAINT `local_resource_form_ibfk_1` FOREIGN KEY (`contact_info_fk`) REFERENCES `contact_info` (`contact_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `local_resource_form_ibfk_2` FOREIGN KEY (`resource_provider_fk`) REFERENCES `resource_provider` (`resource_provider_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `local_resource_form_ibfk_3` FOREIGN KEY (`disaster_fk`) REFERENCES `disaster_type` (`disaster_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `organization_form`
@@ -279,9 +272,10 @@ ALTER TABLE `organization_form`
 ALTER TABLE `volunteer_form`
   ADD CONSTRAINT `volunteer_form_ibfk_1` FOREIGN KEY (`contact_fk`) REFERENCES `contact_info` (`contact_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `volunteer_form_ibfk_4` FOREIGN KEY (`skill_fk`) REFERENCES `skill` (`skill_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `volunteer_form_ibfk_5` FOREIGN KEY (`day_fk`) REFERENCES `day` (`day_id`),
-  ADD CONSTRAINT `volunteer_form_ibfk_6` FOREIGN KEY (`organization_name_fk`) REFERENCES `organization_name` (`org_id`),
-  ADD CONSTRAINT `volunteer_form_ibfk_7` FOREIGN KEY (`resource_fk`) REFERENCES `resource_type` (`resource_id`);
+  ADD CONSTRAINT `volunteer_form_ibfk_5` FOREIGN KEY (`day_fk`) REFERENCES `day` (`day_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `volunteer_form_ibfk_6` FOREIGN KEY (`organization_name_fk`) REFERENCES `organization_name` (`org_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `volunteer_form_ibfk_7` FOREIGN KEY (`resource_fk`) REFERENCES `resource_type` (`resource_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `volunteer_form_ibfk_8` FOREIGN KEY (`address_fk`) REFERENCES `address` (`address_id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
